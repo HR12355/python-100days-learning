f9638b5e166147fde6c25ecfe4db4ced90131909
HR12355 <reitoushokuhin55@gmail.com>
2025-12-23 20:42:30 +0900

    day06: while loop,インデント,robotgame

---
+++ day06/main.py
@@ -0,0 +1,43 @@
+# =====================
+# Day06 学習メモ
+# ======================
+# やったこと
+# コードブロックとインデント
+# 関数、whileループ
+# karel the robot game
+
+# 分かったこと
+# print()　のように名前の後にカッコがあるもの　関数。
+# for   反復処理をする場合。
+# while 目的の条件まで、何度も実行したい場合。
+# Reeborgs worldで練習
+# maze 15日目が終われば、この課題の無限ループにならないものを探してみる。
+
+# 詰まったこと
+# while文の中のif文
+# ======================
+
+# 関数
+def my_function():
+    print("Hello")
+my_function()
+
+# while loop
+# while something_is_true:
+# Reeborgs worldで練習
+# maze 15日目が終われば、この課題の無限ループにならないものを探してみる。
+"""
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if right_is_clear():  
+        turn_right()
+        move()
+    elif front_is_clear(): 
+        move()
+    else:                  
+        turn_left()
+"""
