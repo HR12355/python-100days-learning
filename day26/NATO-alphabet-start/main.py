17c3a0c9a55ac5a9301fd6a4cc5eef446edb5551
HR12355 <reitoushokuhin55@gmail.com>
2026-02-20 16:53:09 +0900

    day26:例外処理を追加

--- day26/NATO-alphabet-start/main.py
+++ day26/NATO-alphabet-start/main.py
@@ -1,12 +1,17 @@
 import pandas
 
-#TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
-
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 new_dict = {value.letter: value.code for (key,value) in data.iterrows()}
-user_inputs = input("Enter a word: ").upper()
-phonetic_code = [new_dict[key] for key in user_inputs]
-print(phonetic_code)
+print(new_dict)
+
+def generate_phonetic():
+    user_inputs = input("Enter a word: ").upper()
+    try:
+        phonetic_code = [new_dict[key] for key in user_inputs]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        generate_phonetic() # 関数内でもう一度関数を呼び出す
+    else:
+        print(phonetic_code)
+
+generate_phonetic()
